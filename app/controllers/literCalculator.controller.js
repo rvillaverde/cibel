cf3c05be596e2b4f3cf114c69b3bfa7ffaac45e5
Romina Villaverde <romina.villaverde@gmail.com>
2016-06-28 17:12:04 -0300

    initial commit

---
+++ app/controllers/literCalculator.controller.js
@@ -0,0 +1,4 @@
+cibelApp.controller('literCalculatorController', ['$scope', function($scope) {
+  $scope.qty = 'cantidad';
+  $scope.cost = 'cost';
+}]);
---
+++ assets/javascript/index.js
@@ -0,0 +1,7 @@
+function openModal() {
+  document.querySelector('.modal-container').classList.remove('hidden');
+}
+
+function closeModal() {
+  document.querySelector('.modal-container').classList.add('hidden');
+}
